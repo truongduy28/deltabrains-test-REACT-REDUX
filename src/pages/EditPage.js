c7e1f4a49cf198b8db050740072a3cad0037f565
truongduy28 <truongduy.vithanhcity@gmail.com>
2023-05-24 00:16:46 +0700

    fix button edit product

--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -148,7 +148,7 @@ const EditPage = () => {
                   type="submit"
                   className="w-full text-white bg-sky-700 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-700 dark:hover:bg-sky-700 dark:focus:ring-primary-800"
                 >
-                  ADD PRODUCT
+                  EDIT PRODUCT
                 </button>
               </form>
             </div>
